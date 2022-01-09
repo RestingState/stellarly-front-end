0ee521f030775175232198e4624818104bd051f6
Denys Zakharkevych <zaharkevich.denis@gmail.com>
2022-01-09 16:50:28 +0200

    updated

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,11 +1,11 @@
-import React from "react";
-import { Link } from "react-router-dom";
+import React from 'react';
+import { Link } from 'react-router-dom';
 // Styles
-import { Wrapper, Logo, SignUpBtn } from "./Header.styles";
+import { Wrapper, Logo, SignUpBtn } from './Header.styles';
 
 const Header = () => (
   <Wrapper>
-    <Link to="/" style={{ textDecoration: "none" }}>
+    <Link to='/' style={{ textDecoration: 'none' }}>
       <Logo>STELLARLY</Logo>
     </Link>
     <SignUpBtn>Sign Up</SignUpBtn>
