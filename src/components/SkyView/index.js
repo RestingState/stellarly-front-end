240d268fdec973b49fa0db8be8d9c97390c6b65d
Denys Zakharkevych <zaharkevich.denis@gmail.com>
2022-01-11 19:15:43 +0200

    added api function for testing

--- src/components/SkyView/index.js
+++ src/components/SkyView/index.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 // Styles
 import { Wrapper, NavPanel } from "./SkyView.styles";
 // Components
@@ -7,6 +7,8 @@ import SettingsMenuIcon from "../SettingsMenuIcon";
 import Toggle from "../Toggle";
 import SkyViewInfoMenu from "../SkyViewInfoMenu";
 import SkyViewSettingsMenu from "../SkyViewSettingsMenu";
+// API
+import fetchStars from "../../api/satellitesAPI";
 
 const SkyView = () => {
   const [isActiveInfoMenu, setIsActiveInfoMenu] = useState(false);
@@ -22,6 +24,19 @@ const SkyView = () => {
     setIsActiveSettingsMenu(bool);
     setIsActiveMenu(bool);
   };
+
+  useEffect(() => {
+    const getStars = async () => {
+      // setError(false);
+      try {
+        const response = await fetchStars();
+        console.log(response);
+      } catch (e) {
+        console.log(e);
+      }
+    };
+    getStars();
+  }, []);
 
   return (
     <Wrapper>
