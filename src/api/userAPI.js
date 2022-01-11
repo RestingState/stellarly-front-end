21195955427ed4ee4cf225cd58e7fa93a909c738
Denys Zakharkevych <zaharkevich.denis@gmail.com>
2022-01-11 19:14:47 +0200

    added api handlers

---
+++ src/api/userAPI.js
@@ -0,0 +1,12 @@
+import { REGISTRATION_URL, LOGIN_URL, GET_USER_INFO_URL } from "../config";
+import { $api } from "./axios";
+
+const createUser = async (data) => {
+  return await $api.post(`${REGISTRATION_URL}`, data);
+};
+
+// const loginUser = async (data) => {
+//   return await $api.get(`${LOGIN_URL}`, )
+// }
+
+export default createUser;
---
+++ src/config.js
@@ -0,0 +1,18 @@
+const API_URL = "http://127.0.0.1:5000";
+
+const REGISTRATION_URL = `${API_URL}/user`;
+const LOGIN_URL = `${API_URL}/user/login`;
+const GET_USER_INFO_URL = `${API_URL}/user`;
+const FETCH_STARS_URL = `${API_URL}/stars`;
+const FETCH_SATELLITES_URL = `${API_URL}/satellites`;
+const FETCH_WEATHER_URL = `${API_URL}/weather`;
+
+export {
+  API_URL,
+  REGISTRATION_URL,
+  LOGIN_URL,
+  GET_USER_INFO_URL,
+  FETCH_STARS_URL,
+  FETCH_SATELLITES_URL,
+  FETCH_WEATHER_URL,
+};
