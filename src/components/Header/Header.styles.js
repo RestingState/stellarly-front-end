1afc73bd0a52f060825fec1ab6317e316ea7fea4
Denys Zakharkevych <zaharkevich.denis@gmail.com>
2022-01-15 17:42:53 +0200

    added header disappearence and pages references

--- src/components/Header/Header.styles.js
+++ src/components/Header/Header.styles.js
@@ -1,12 +1,23 @@
-import styled from "styled-components";
+import styled, { css } from "styled-components";
 
 export const Wrapper = styled.div`
+  position: fixed;
+  top: 0;
+  left: 0;
+  width: 100%;
+  z-index: 10000;
   display: flex;
   align-items: center;
   justify-content: space-between;
   background: var(--darkGrey);
   color: var(--white);
-  padding: 0.8em 8em;
+  padding: 0.8em 3em;
+
+  ${(props) =>
+    !props.active &&
+    css`
+      display: none;
+    `}
 
   @media (max-width: 45em) {
     padding: 0.8em 2em;
@@ -16,28 +27,18 @@ export const Wrapper = styled.div`
 export const Logo = styled.span`
   cursor: pointer;
   font-size: 2rem;
-  color: var(--white);
 
   @media (max-width: 45em) {
     font-size: 1.5rem;
   }
 `;
 
-export const SignUpBtn = styled.div`
-  display: inline-block;
-  padding: 0.5em 1.5em;
-  font-weight: 600;
-  border: 1px solid var(--white);
-  cursor: pointer;
-  transition: transform 0.5s ease;
-
-  :hover {
-    transition: all 0.3s ease-out;
-    background: var(--white);
-    color: var(--darkGrey);
-  }
-
-  @media (max-width: 45em) {
-    padding: 0.3em 0.9em;
+export const NavBar = styled.nav`
+  ul {
+    cursor: pointer;
+    display: flex;
+    list-style: none;
+    gap: 6rem;
+    font-size: 1.4rem;
   }
 `;
--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,25 +1,40 @@
 import React, { useState } from "react";
 import { Link } from "react-router-dom";
 // Styles
-import { Wrapper, Logo, SignUpBtn } from "./Header.styles";
+import { Wrapper, Logo, NavBar } from "./Header.styles";
 
 const Header = () => {
+  const [activeHeader, setActiveHeader] = useState(false);
   const [userIsAuth, setUserIsAuth] = useState(true);
 
+  const handleHeader = () => {
+    if (window.scrollY > 1) {
+      setActiveHeader(true);
+    } else {
+      setActiveHeader(false);
+    }
+  };
+
+  window.addEventListener("scroll", handleHeader);
+
   return (
-    <Wrapper>
-      <Link to="/" style={{ textDecoration: "none" }}>
+    <Wrapper active={activeHeader}>
+      <Link to="/">
         <Logo>STELLARLY</Logo>
       </Link>
-      {userIsAuth ? (
-        <Link to="/account" style={{ textDecoration: "none" }}>
-          <h3>My account</h3>
-        </Link>
-      ) : (
-        <Link to="/registration" style={{ textDecoration: "none" }}>
-          <SignUpBtn>Sign Up</SignUpBtn>
-        </Link>
-      )}
+      <NavBar>
+        <ul>
+          <li>
+            <Link to="/">Home</Link>
+          </li>
+          <li>
+            <Link to="/account">Personal page</Link>
+          </li>
+          <li>
+            <Link to="/contact">Contact us</Link>
+          </li>
+        </ul>
+      </NavBar>
     </Wrapper>
   );
 };
