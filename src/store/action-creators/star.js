2d5b18cc6f95f7714fb8249514939277820141a2
Denys Zakharkevych <zaharkevich.denis@gmail.com>
2022-01-19 11:20:46 +0200

    added new functions

--- src/store/action-creators/star.js
+++ src/store/action-creators/star.js
@@ -2,20 +2,32 @@ import { $api } from "../../api/axios";
 import { FETCH_STARS_URL } from "../../config";
 import { StarActionTypes } from "../../types/star";
 
-export const fetchStars = () => {
+const fetchStarsAction = () => {
+  return { type: StarActionTypes.FETCH_STARS };
+};
+
+const fetchStarsSuccessAction = (payload) => {
+  return {
+    type: StarActionTypes.FETCH_STARS_SUCCESS,
+    payload,
+  };
+};
+
+const fetchStarsErrorAction = (payload) => {
+  return {
+    type: StarActionTypes.FETCH_STARS_ERROR,
+    payload,
+  };
+};
+
+export const fetchStars = (limit = 10) => {
   return async (dispatch) => {
     try {
-      dispatch({ type: StarActionTypes.FETCH_STARS });
-      const response = await $api.get(`${FETCH_STARS_URL}?limit=10`);
-      dispatch({
-        type: StarActionTypes.FETCH_STARS_SUCCESS,
-        payload: response.data,
-      });
+      dispatch(fetchStarsAction());
+      const response = await $api.get(`${FETCH_STARS_URL}?limit=${limit}`);
+      dispatch(fetchStarsSuccessAction(response.data));
     } catch (e) {
-      dispatch({
-        type: StarActionTypes.FETCH_STARS_ERROR,
-        payload: "Error during star fetching",
-      });
+      dispatch(fetchStarsErrorAction("Error during star fetching"));
     }
   };
 };
