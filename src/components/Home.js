50831cb13b4f75e65bc1d39cb244f7e13900b9ae
Denys Zakharkevych <zaharkevich.denis@gmail.com>
2022-01-19 14:14:31 +0200

    added resource section

--- src/components/Home.js
+++ src/components/Home.js
@@ -2,12 +2,14 @@ import React from "react";
 // Components
 import SkyView from "./SkyView";
 import AboutUsSection from "./AboutUsSection";
+import ResourcesSection from "./ResourcesSection";
 
 const Home = () => {
   return (
     <>
       <SkyView />
       <AboutUsSection />
+      <ResourcesSection />
     </>
   );
 };
