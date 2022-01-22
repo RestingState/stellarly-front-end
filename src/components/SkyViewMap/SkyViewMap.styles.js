710c491b8fc1c3ad8141a7ef582c9a35e0c15af4
Denys Zakharkevych <zaharkevich.denis@gmail.com>
2022-01-22 16:54:57 +0200

    css for spinner

--- src/components/SkyViewMap/SkyViewMap.styles.js
+++ src/components/SkyViewMap/SkyViewMap.styles.js
@@ -1,7 +1,16 @@
 import styled from "styled-components";
 
+export const Wrapper = styled.div``;
+
 export const Map = styled.canvas`
   display: block;
-  /* position: relative;
-  z-index: 0; */
+`;
+
+export const SpinnerBox = styled.div`
+  position: absolute;
+  top: 50%;
+  left: 50%;
+  margin-top: -40px;
+  margin-left: -40px;
+  z-index: 100000;
 `;
