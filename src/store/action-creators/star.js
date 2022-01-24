951cb4ed3bbadd160f9263d3e55a7a9bf1f55718
Denys Zakharkevych <zaharkevich.denis@gmail.com>
2022-01-24 12:44:20 +0200

    changed star fetching logic

--- src/store/action-creators/star.js
+++ src/store/action-creators/star.js
@@ -1,6 +1,7 @@
 import { $api } from '../../api/axios';
 import { FETCH_STARS_URL } from '../../config';
 import { StarActionTypes } from '../../types/star';
+import { getStarsCoordinates } from '../../components/SkyViewMap/helpers';
 
 const fetchStarsAction = () => {
   return { type: StarActionTypes.FETCH_STARS };
@@ -25,7 +26,9 @@ export const fetchStars = (limit = 10) => {
     try {
       dispatch(fetchStarsAction());
       const response = await $api.get(`${FETCH_STARS_URL}?limit=${limit}`);
+      response.data = getStarsCoordinates(response.data);
       dispatch(fetchStarsSuccessAction(response.data));
+      return Promise.resolve(response.data);
     } catch (e) {
       dispatch(fetchStarsErrorAction('Error during star fetching'));
     }
