f1f53772ff67e7f1a26ff016ea3795ca6b45a4d3
Denys Zakharkevych <zaharkevich.denis@gmail.com>
2022-01-24 12:39:42 +0200

    added fetch planet url

--- src/config.js
+++ src/config.js
@@ -5,6 +5,7 @@ const LOGIN_URL = `${API_URL}/user/login`;
 const GET_USER_INFO_URL = `${API_URL}/user`;
 const FETCH_STARS_URL = `${API_URL}/stars`;
 const FETCH_SATELLITES_URL = `${API_URL}/satellites`;
+const FETCH_PLANETS_URL = `${API_URL}/planets`;
 const FETCH_WEATHER_URL = `${API_URL}/weather`;
 
 export {
@@ -14,5 +15,6 @@ export {
   GET_USER_INFO_URL,
   FETCH_STARS_URL,
   FETCH_SATELLITES_URL,
+  FETCH_PLANETS_URL,
   FETCH_WEATHER_URL
 };
