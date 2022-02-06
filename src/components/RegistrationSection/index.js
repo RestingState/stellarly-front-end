8a1e9d416eb94bf3ba603bf72425c05997739271
Denys Zakharkevych <zaharkevich.denis@gmail.com>
2022-02-06 13:47:05 +0200

    modified registration page

--- src/components/RegistrationSection/index.js
+++ src/components/RegistrationSection/index.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState, useRef } from 'react';
 // Styles
 import {
   Content,
@@ -15,8 +15,72 @@ import {
 } from './RegistrationSection.styles';
 // Components
 import Button from '@mui/material/Button';
+import ErrorPopup from '../ErrorPopup';
+// User API
+import { createUser, loginUser } from '../../api/userAPI';
 
-const RegistrationSection = () => {
+const RegistrationSection = ({ userData, setUserData }) => {
+  const [errorPopupActive, setErrorPopupActive] = useState(false);
+  const errorMessage = useRef('');
+
+  const setFirstName = (e) => {
+    const fname = e.target.value;
+    setUserData({ ...userData, fname });
+  };
+
+  const setSecondName = (e) => {
+    const sname = e.target.value;
+    setUserData({ ...userData, sname });
+  };
+
+  const setUsername = (e) => {
+    const username = e.target.value;
+    setUserData({ ...userData, username });
+  };
+
+  const setEmail = (e) => {
+    const email = e.target.value;
+    setUserData({ ...userData, email });
+  };
+
+  const setPassword = (e) => {
+    const password = e.target.value;
+    setUserData({ ...userData, password });
+  };
+
+  const setCity = (e) => {
+    const city_id = e.target.value;
+    setUserData({ ...userData, city_id });
+  };
+
+  const existEmptyFields = () => {
+    if (
+      !userData.fname ||
+      !userData.sname ||
+      !userData.username ||
+      !userData.email ||
+      !userData.password ||
+      !userData.city_id
+    ) {
+      return true;
+    }
+    return false;
+  };
+
+  const register = async () => {
+    if (existEmptyFields()) {
+      errorMessage.current = 'all fields has to be filled';
+      setErrorPopupActive(true);
+      return;
+    }
+
+    try {
+      const response = await createUser(userData);
+    } catch (e) {
+      console.log(e);
+    }
+  };
+
   return (
     <Wrapper>
       <Content>
@@ -30,39 +94,64 @@ const RegistrationSection = () => {
             <Fields>
               <Field>
                 <FieldName>First name:</FieldName>
-                <Input type="text" />
+                <Input
+                  type="text"
+                  value={userData.fname}
+                  onChange={setFirstName}
+                />
               </Field>
               <Field>
                 <FieldName>Last name:</FieldName>
-                <Input type="text" />
+                <Input
+                  type="text"
+                  value={userData.sname}
+                  onChange={setSecondName}
+                />
               </Field>
               <Field>
                 <FieldName>Username:</FieldName>
-                <Input type="text" />
+                <Input
+                  type="text"
+                  value={userData.username}
+                  onChange={setUsername}
+                />
               </Field>
               <Field>
                 <FieldName>E-mail:</FieldName>
-                <Input type="text" />
+                <Input type="text" value={userData.email} onChange={setEmail} />
               </Field>
               <Field>
                 <FieldName>Password:</FieldName>
-                <Input type="text" />
+                <Input
+                  type="text"
+                  value={userData.password}
+                  onChange={setPassword}
+                />
               </Field>
             </Fields>
             <Fields>
               <Field>
                 <FieldName>City:</FieldName>
-                <Input type="text" />
+                <Input
+                  type="text"
+                  value={userData.city_id}
+                  onChange={setCity}
+                />
               </Field>
             </Fields>
             <BtnWrapper>
-              <Button variant="contained" color="error">
+              <Button variant="contained" color="error" onClick={register}>
                 Registration
               </Button>
             </BtnWrapper>
           </FieldForm>
         </RegistrationForm>
       </Content>
+      <ErrorPopup
+        active={errorPopupActive}
+        setActive={setErrorPopupActive}
+        message={errorMessage.current}
+      />
     </Wrapper>
   );
 };
