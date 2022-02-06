b0013d272c57da5940e2f0ada2b1c023a550919b
Denys Zakharkevych <zaharkevich.denis@gmail.com>
2022-02-06 13:47:43 +0200

    created personal info section

--- src/components/UserPersonalInfoSection/UserPersonalInfoSection.styles.js
+++ src/components/UserPersonalInfoSection/UserPersonalInfoSection.styles.js
@@ -1,7 +1,7 @@
 import styled from 'styled-components';
 
 export const Wrapper = styled.div`
-  padding: 4rem 0 5rem 0;
+  padding: 10vh 0 15vh 15vw;
   background-color: #101010;
   color: white;
 
@@ -12,6 +12,43 @@ export const Wrapper = styled.div`
 
 export const Content = styled.div``;
 
-export const Title = styled.div``;
+export const Title = styled.div`
+  font-size: 2.5rem;
 
-export const Description = styled.div``;
+  @media (max-width: 40rem) {
+    font-size: 1.5rem;
+  }
+`;
+
+export const Description = styled.div`
+  font-size: 1.5rem;
+  padding: 1.5rem 0;
+
+  @media (max-width: 40rem) {
+    font-size: 1.1rem;
+    padding: 0.8rem 0;
+  }
+`;
+
+export const FieldForm = styled.div`
+  display: flex;
+  justify-content: center;
+`;
+
+export const Fields = styled.div`
+  display: flex;
+  flex-wrap: wrap;
+`;
+
+export const Field = styled.div`
+  padding: 1rem 0;
+  font-size: 1.3rem;
+  flex-basis: 50%;
+`;
+
+export const Ref = styled.div`
+  text-decoration: underline;
+  padding: 1rem 0;
+  font-size: 1.3rem;
+  flex-basis: 50%;
+`;
--- src/components/UserPersonalInfoSection/index.js
+++ src/components/UserPersonalInfoSection/index.js
@@ -1,21 +1,84 @@
-import React from 'react';
+import React, { useState, useEffect, useRef } from 'react';
+// Styles
 import {
   Content,
   Title,
   Wrapper,
-  Description
+  Description,
+  FieldForm,
+  Fields,
+  Field,
+  Ref
 } from './UserPersonalInfoSection.styles';
+// Components
+import LoginForm from '../LoginForm';
+import ErrorPopup from '../ErrorPopup';
+// API
+import { getUserInfo } from '../../api/userAPI';
 
 const UserPersonalInfoSection = () => {
+  const [loginFormActive, setLoginFormActive] = useState(false);
+  const [errorPopupActive, setErrorPopupActive] = useState(false);
+  const [userData, setUserData] = useState({
+    first_name: '',
+    last_name: '',
+    username: '',
+    email: '',
+    city_id: ''
+  });
+  const errorMessageRef = useRef('');
+
+  useEffect(() => {
+    const token = sessionStorage.getItem('user_token');
+    const getUserData = async () => {
+      try {
+        const response = await getUserInfo(token);
+        if (response.status === 422) {
+          setLoginFormActive(true);
+          return;
+        }
+        if (response.status !== 200) {
+          throw Error;
+        }
+
+        console.log(response.data);
+        setUserData(response.data);
+        return response.data;
+      } catch (e) {
+        errorMessageRef.current = 'Error';
+        setErrorPopupActive(true);
+      }
+    };
+    getUserData();
+  }, []);
+
   return (
-    <Wrapper>
-      <Content>
-        <Title>This is your personal page</Title>
-        <Description>
-          Configure your personal info and set notifications here.
-        </Description>
-      </Content>
-    </Wrapper>
+    <>
+      <LoginForm active={loginFormActive} />
+      <ErrorPopup
+        active={errorPopupActive}
+        setActive={setErrorPopupActive}
+        message={errorMessageRef.current}
+      />
+      <Wrapper>
+        <Content>
+          <Title>This is your personal page</Title>
+          <Description>
+            Configure your personal info and set notifications here.
+          </Description>
+          <FieldForm>
+            <Fields>
+              <Field>First name: {userData.first_name}</Field>
+              <Field>Last name: {userData.last_name}</Field>
+              <Field>Username: {userData.username}</Field>
+              <Field>E-mail: {userData.email}</Field>
+              <Field>City: {userData.city_id}</Field>
+              <Ref>Change Password</Ref>
+            </Fields>
+          </FieldForm>
+        </Content>
+      </Wrapper>
+    </>
   );
 };
 
