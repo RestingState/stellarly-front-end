2e187c003bbe2d04755c89efd8b46058e6ea675b
Denys Zakharkevych <zaharkevich.denis@gmail.com>
2022-02-06 21:07:23 +0200

    added login form

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,29 +1,63 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
+import React, { useState } from 'react';
+import { Link, useNavigate } from 'react-router-dom';
 // Styles
 import { Wrapper, Logo, NavBar } from './Header.styles';
 // Components
+import LoginForm from '../LoginForm';
+// API
+import { getUserInfo } from '../../api/userAPI';
 
 const Header = ({ active, fixed = false }) => {
+  const navigate = useNavigate();
+  const [loginFormActive, setLoginFormActive] = useState(false);
+
+  const isAuth = async () => {
+    try {
+      const response = await getUserInfo();
+      if (response.status === 200) {
+        return true;
+      }
+    } catch (e) {
+      return false;
+    }
+  };
+
+  const handlePersonalPageLink = () => {
+    isAuth()
+      .then((response) => {
+        if (response) {
+          navigate('/account');
+        } else {
+          setLoginFormActive(true);
+        }
+      })
+      .catch(() => {
+        setLoginFormActive(true);
+      });
+  };
+
   return (
-    <Wrapper active={active} fixed={fixed}>
-      <Link to="/">
-        <Logo>STELLARLY</Logo>
-      </Link>
-      <NavBar>
-        <ul>
-          <li>
-            <Link to="/">Home</Link>
-          </li>
-          <li>
-            <Link to="/account">Personal page</Link>
-          </li>
-          <li>
-            <Link to="/contact">Contact us</Link>
-          </li>
-        </ul>
-      </NavBar>
-    </Wrapper>
+    <>
+      <LoginForm active={loginFormActive} setActive={setLoginFormActive} />
+      <Wrapper active={active} fixed={fixed}>
+        <Link to="/">
+          <Logo>STELLARLY</Logo>
+        </Link>
+        <NavBar>
+          <ul>
+            <li>
+              <Link to="/">Home</Link>
+            </li>
+            <li>
+              <span onClick={handlePersonalPageLink}>Personal page</span>
+            </li>
+            <li>
+              <Link to="/contact">Contact us</Link>
+            </li>
+          </ul>
+        </NavBar>
+      </Wrapper>
+    </>
   );
 };
 
